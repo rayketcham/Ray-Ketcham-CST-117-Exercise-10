d30464dc9abe6e549fdcfaa17984dc2303932e47
rayketcham <rayketcham@ogjos.com>
2018-08-05 14:13:01 -0500

    Add project files.

---
+++ Ray-Ketcham-CST-117-Exercise-10/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ray_Ketcham_CST_117_Exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
